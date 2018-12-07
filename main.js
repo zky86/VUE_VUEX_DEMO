9c7fe5d56153361272544d37fb892e32d98e644a
汤圆 <272495222@qq.com>
2018-12-07 09:19:17 +0800

    更新

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import store from './vuex/store'
 import App from './components/App.vue'
+import testmodelpub from 'testmodelpub'
+console.log(345);
 
+console.log(testmodelpub);
 new Vue({
   store, 
   el: '#app',
